7e17b6c7b6ecba506330c3dbe36e51b424c030ac
202103382_진주연 <101190159+Est0Z@users.noreply.github.com>
2025-06-06 13:19:43 +0900

    Delete components directory

--- components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Main } from './main/main';
-export { default as Calendar } from './calendar/calendar';
-export { default as Stats } from './stats/stats';
-export { default as Today } from './today/today';
-export { default as FavPoems } from './favPoems/favPoems';
-export { default as LoginPage } from './login/login';
-export { default as SignupPage } from './signup/signup';
