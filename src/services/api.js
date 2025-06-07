8ec15b0bc62ffc243dce889fface1446fa8ee9bb
202103382_진주연 <101190159+Est0Z@users.noreply.github.com>
2025-06-07 21:55:07 +0900

    Add files via upload

--- src/services/api.js
+++ src/services/api.js
@@ -4,15 +4,24 @@ import axios from 'axios';
 
 const BASE_URL = 'http://localhost:3000/api/today'; 
 
-export const saveDiaryDB = async (diary) => {
+export const saveDiaryDB = async (diary, emotionType) => {
       const token = localStorage.getItem("accessToken"); 
       // const userId = localStorage.getItem("user_id");
       const userId = 1;
       console.log('saveDiaryDB 호출, accessToken:', token);
+
+      const dataToSend = {
+        user_id: userId,
+        content: diary,
+        emotion_type: emotionType,
+      };
+      console.log('백엔드로 보낼 데이터:', dataToSend);
+
   try {
     const response = await axios.post(`${BASE_URL}/diaries`, {
       content: diary,
-      user_id: userId, }, {
+      user_id: userId,
+      emotion_type: emotionType, }, {
       headers: {
         Authorization: `Bearer ${token}`, 
         'Content-Type': 'application/json',
@@ -28,10 +37,13 @@ export const saveDiaryDB = async (diary) => {
   }
 };
 
-export const generatePoem = async (content, diary_id) => {
+export const generatePoem = async (content, diary_id, emotion_type) => {
   try {
     console.log('generatePoem 호출, diary_id:', diary_id);
-    const response = await axios.post(`${BASE_URL}/poemphrase`, { content, diary_id }, {
+    const response = await axios.post(`${BASE_URL}/poemphrase`, { 
+      content, 
+      diary_id, 
+      emotion_type }, {
       headers: {
         'Content-Type': 'application/json',
       },
@@ -48,7 +60,6 @@ export const savePoem = async (user_id, diary_id, poem, emotion_id) => {
   try {
     console.log('savePoem 호출, user_id:', user_id, 'diary_id:', diary_id, 'poem:', poem);
     const response = await axios.post(`${BASE_URL}/poems`, {
-      user_id,
       diary_id,
       poem: poem, 
       emotion_id: emotion_id
@@ -75,8 +86,8 @@ export const getLikedPoems = async (user_id) => {
 // 시 좋아요/취소 2025.06.01
 export const likePoem = async (poemId, liked) => {
   try {
-    const response = await axios.post('http://localhost:3000/api/calendar/poem/like', {
-      poemId,
+    const response = await axios.post('http://localhost:3000/api/today/likepoem', {
+      poem_id: poemId,
       liked,
     }, {
       headers: {
